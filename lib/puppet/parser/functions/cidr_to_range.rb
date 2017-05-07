2da5e2e9328a10e6a3c97f1b6e506549cee33f9c
Ray Ward <ray@rayward.com.au>
2017-05-08 09:01:34 +1000

    Code style fixes

--- lib/puppet/parser/functions/cidr_to_range.rb
+++ lib/puppet/parser/functions/cidr_to_range.rb
@@ -7,22 +7,22 @@ module Puppet::Parser::Functions
     ENDHEREDOC
     ) do |args|
 
-    unless args.length == 1 then
+    unless args.length == 1
       raise Puppet::ParseError, ("cidr_to_range(): wrong number of arguments (#{args.length}; must be 1)")
     end
+
     arg = args[0]
-    unless arg.respond_to?('to_s') then
+    unless arg.respond_to?('to_s')
       raise Puppet::ParseError, ("#{arg.inspect} is not a string. It looks to be a #{arg.class}")
     end
 
-    cidr = nil
     begin
       cidr = IPAddr.new(arg.to_s)
     rescue ArgumentError => e
       raise Puppet::ParseError, (e)
     end
 
-    ips = cidr.to_range.each.map { |ip| ip.to_s }
+    ips = cidr.to_range.map { |ip| ip.to_s }
     ips.shift
     ips.pop
     ips
